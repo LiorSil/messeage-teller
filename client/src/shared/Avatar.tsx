c0febdcabe400e74f182a8802dea8b47f798d485
Lior Silman <lior.silmansoad@gmail.com>
2025-04-05 22:26:45 +0300

    refactor: Remove default size prop and set fixed dimensions for avatar component

--- client/src/shared/Avatar.tsx
+++ client/src/shared/Avatar.tsx
@@ -12,14 +12,12 @@ interface AvatarProps {
 }
 
 // Default values
-const DEFAULT_SIZE = 24;
 const DEFAULT_AVATAR = "https://flowbite.com/docs/images/logo.svg";
 const DEFAULT_NAME = "Guest";
 
 const Avatar: React.FC<AvatarProps> = ({
   avatarUrl,
   name,
-  size = DEFAULT_SIZE, 
   fallbackAvatarUrl = DEFAULT_AVATAR,
   className = "", 
   href, 
@@ -28,7 +26,7 @@ const Avatar: React.FC<AvatarProps> = ({
   const finalAvatarUrl = avatarUrl || fallbackAvatarUrl;
   const finalName = name;
 
-  const sizeClasses = `w-${size} h-${size}`;
+
   
 
   // Construct the common classes for the container
@@ -41,7 +39,7 @@ const Avatar: React.FC<AvatarProps> = ({
     <a href={href} className={containerClasses}>
       <img
         src={finalAvatarUrl}
-        className={`${sizeClasses} rounded-full border-2 border-black object-cover object-center`}
+        className={`h-14 w-14 rounded-full border-2 border-black object-cover object-center`}
         alt={finalName || ""} // More descriptive alt text
         onError={(e) => {
                     if (e.currentTarget.src !== fallbackAvatarUrl) {
